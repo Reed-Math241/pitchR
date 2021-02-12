9917cf6a25fe8b3e16638cb374445a88fc7d94da
Andy Jesse Zhao <andjzhao@rstudio.reed.edu>
2021-02-11 16:33:19 -0800

    updated help doc

--- R/pitchR.R
+++ R/pitchR.R
@@ -2,7 +2,10 @@
 #' 
 #' Basic counting statistics, advanced, Sabermetric analytics, and salaries for starting MLB pitchers from 2015-2019.
 #' 
-#' @format A tibble with 457 rows and 36 variables:
+#'@docType data
+#' @name pitchR
+#' @usage data(pitchR)
+#' @format A tibble with 457 rows and 31 variables:
 #' \describe{
 #' \item{\code{Pitches}}{ Total number of pitches thrown}
 #' \item{\code{Pitcher}}{ Pitcher name}
@@ -28,21 +31,15 @@
 #' \item{\code{BB_percent}}{ Percent of batters walked on balls}
 #' \item{\code{hard_hit_percent}}{ Percentage of pitches that resulted a "hard hit" by statcast's definition (exit velocity of >95 mph)}
 #' \item{\code{barrel_percent}}{ Percentage of pitches that resulted in a "barrel", which statcast defines as a hit ball with an exit velocity and launch angle that have led to a minimum .500 batting average and 1.500 slugging percentage historically}
-#' \item{\code{ERA_t1}}{ ERA in the next season}
 #' \item{\code{Salary	}}{ Salary (US dollars)}
-#' \item{\code{salary_t1	}}{ Salary of the next season}
 #' \item{\code{BFP_per_G	}}{ Batters faced per game}
 #' \item{\code{BABIP_minus_meanBABIP	}}{ Pitcher's BABIP subtracted by the mean BABIP of the league, meant to represent a quantification of the pitcher's luck}
 #' \item{\code{xwOBA_minus_wOBA}}{ Pitcher's statcast expected wOBA minus observed wOBA, also a luck-reversion variable}
 #' \item{\code{ERA/Barrel %	}}{ Pitcher ERA divided by barrel %, also a luck-reversion variable attempting to connect the amount of hard contact given up with the amount of runs allowed}
 #' \item{\code{ERA/Hard Hit %	}}{ Pitcher ERA divided by hard hit %, yet another luck-reversion variable similar to ERA/barrel %}
-#' \item{\code{ΔERA	}}{ Change in pitcher ERA from current season to the next}
-#' \item{\code{ΔSalary	}}{ Change in pitcher salary from current season to the next}
-#' \item{\code{luck_adj_ERA	}}{ Raw pitcher ERA adjusted with variously-weighted luck-reversion statistics}
 #' }
 #' 
 #' 
-#' @source {Data collated by Riley Leonard, Andy Zhao, and Josh Yamamoto and used in the "Math 241" final project in order to model future ERA and salary based off current statistics. The basic counting statistics was taken from Sean Lahman's baseball database, Statcast data was downloaded via baseballsavant, the salary was scraped from Sportrac.} 
-#' @source {For a more in-depth explanation of many of these variables see: } \url{<http://m.mlb.com/glossary/>}
+#' @source {Data collated by Riley Leonard, Andy Zhao, and Josh Yamamoto and used in the "Math 241" final project in order to model future ERA and salary based off current statistics. The basic counting statistics was taken from Sean Lahman's baseball database, Statcast data was downloaded via baseballsavant, the salary was scraped from Sportrac. For a more in-depth explanation of many of these variables see} \url{<http://m.mlb.com/glossary/>}
 #' 
- "pitchR"
+"pitchR"
